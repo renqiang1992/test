eca53fc7500cef1cc37ae50440766361f02fb3c9
renqiang <1601263893@qq.com>
2020-05-12 15:47:57 +0800

    second commit

--- spring-cloud-config-eureka-bus/spring-cloud-config-server/src/main/java/com/neo/ConfigServerApplication.java
+++ spring-cloud-config-eureka-bus/spring-cloud-config-server/src/main/java/com/neo/ConfigServerApplication.java
@@ -13,5 +13,6 @@ public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+		System.out.println("启动成功。。。。。。。。。。");
 	}
 }
--- spring-cloud-config-svn-refresh/spring-cloud-config-client-refresh/src/main/java/com/neo/ConfigClientApplication.java
+++ spring-cloud-config-svn-refresh/spring-cloud-config-client-refresh/src/main/java/com/neo/ConfigClientApplication.java
@@ -3,11 +3,14 @@ package com.neo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
+		System.out.println("启动成功。。。。。。。。。。");
 	}
 }
