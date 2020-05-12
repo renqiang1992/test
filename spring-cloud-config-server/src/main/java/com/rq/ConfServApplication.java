5bfbd4aa72a29ba2e7362c30083842e859a9e570
renqiang <1601263893@qq.com>
2020-05-12 20:41:03 +0800

    asd

--- spring-cloud-config-server/src/main/java/com/rq/ConfServApplication.java
+++ spring-cloud-config-server/src/main/java/com/rq/ConfServApplication.java
@@ -5,14 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer//启用config配置中心
-@EnableDiscoveryClient//启用服务注册与发现
+@EnableConfigServer//锟斤拷锟斤拷config锟斤拷锟斤拷锟斤拷锟斤拷
+@EnableDiscoveryClient//锟斤拷锟矫凤拷锟斤拷注锟斤拷锟诫发锟斤拷
 @SpringBootApplication
 public class ConfServApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfServApplication.class, args);
-		System.out.println("配置中心服务端启动成功!");
+		System.out.println("ConfServApplication success!");
 
 	}
 
